8b37b9f00ffcec65e98d0d589bc325fde12d6280
Timothy Pham <tpham2580@gmail.com>
2021-01-14 22:29:35 -0600

    added country image amount and country 3 letter code js files

--- js/countries.js
+++ js/countries.js
@@ -1,35 +1,55 @@
 // js is for all country htmls
 // will generate page depending on each country
 
-var get_countries =   {
-    "vietnam": "VNM", 
-    //"USA": "united-states.html",
-    //"JPN": "united-states.html",
-    //"IND": "united-states.html",
-    };
+import * as c_image_amounts from "./country_image_amounts.js"
+import * as countries_3_letter from "./countries_3_letter.js"
+
+
 
 // gets the country name from the path
 var path = window.location.pathname;
 var page = path.split("/").pop();
 var name_country = page.split('.').slice(0, -1).join('.').toString();
 
-var fetch_url = "https://foodmap-backend-nxjye5q3fq-uc.a.run.app/country/" + get_countries[name_country];
+var fetch_url = "https://foodmap-backend-nxjye5q3fq-uc.a.run.app/country/" + countries_3_letter.get_countries[name_country];
 
 // fetch response to get json data of all dishes from specified country
 fetch(fetch_url).then(response => response.json()).then(json => {
                 main(json);
         }).catch(err => console.log(err));
 
-function picture_carousel(name){
+// gets the main container by id "main"
+var main_container = document.getElementById("main");
 
+function picture_carousel(dish){
+    var new_col = document.createElement("div");
+    new_col.setAttribute("class", "col-6");
+
+    var dish_name = dish["name"];
+    console.log(dish_name)
+    var amount_images = c_image_amounts.country_images[name_country][dish_name.toString()];
+
+    //for loop to add all dish images to carousel
+    for (var image=0; image<amount_images+1;image++){
+        //images url to add to carousel
+        //var image_url = "../images/" + name_country + "/" + dish_name;
+    }
+        
+
+    return new_col
 }
 
-function create_section(country_data){
-    console.log(country_data);
+function create_section(dish_data){
+    var new_row = document.createElement("div");
+    new_row.setAttribute("class", "row");
+
+    new_row.appendChild(picture_carousel(dish_data));
+
+    
 }
 
 function main(data){
-    console.log(data);
+
     var data_length = Object.keys(data).length;
     for (var i=0; i<data_length; i++){
         create_section(data[i]);
---
+++ js/country_image_amounts.js
@@ -0,0 +1,11 @@
+// js file for number of images for each dish
+
+export var country_images = {
+    "vietnam": {
+        "Bun Bo Hue": 3,
+        "Goi Cuon": 6,
+        "Pho": 5,
+        "Banh Xeo": 2,
+    },
+
+};
