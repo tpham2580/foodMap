2702d8a86605d0c2696f1b74788eb4193612ea1e
Timothy Pham <tpham2580@gmail.com>
2021-01-20 23:40:13 -0600

    added styling and all dish information

--- js/countries.js
+++ js/countries.js
@@ -24,7 +24,7 @@ var main_container = document.getElementById("main");
 // creates picture_carousel
 function picture_carousel(dish){
     var new_col = document.createElement("div");
-    new_col.setAttribute("class", "col-6");
+    new_col.setAttribute("class", "col-xxl-6 col-xl-12 m-auto p-0 align-center");
 
     var dish_name = dish["name"];
     var dish_name_w_dashes = dish_name.replace(/\s+/g, "-");
@@ -38,27 +38,6 @@ function picture_carousel(dish){
     food_carousel.setAttribute("class", "carousel slide");
     food_carousel.setAttribute("data-bs-ride", "carousel");
     
-        // create ol with class=class="carousel-indicators"
-        var ol_carousel = document.createElement("ol");
-            // for slide_number in range 0 to amount_images
-            for (var slide_number=0; slide_number<amount_images; slide_number++){
-                // create li with data-bs-target="#foodCarousel-" + dish_name_w_dashes ata-bs-slide-to=slide_number
-                var li_carousel = document.createElement("li");
-                li_carousel.setAttribute("data-bs-target", "#foodCarousel-" + dish_name_w_dashes);
-                li_carousel.setAttribute("ata-bs-slide-to", slide_number.toString());
-
-                // if slide_number == 0, add class="active"
-                if (slide_number==0){
-                    li_carousel.setAttribute("class", "active");
-                }
-
-                // append to ol
-                ol_carousel.appendChild(li_carousel);
-            }
-                
-        // append ol to div
-        food_carousel.appendChild(ol_carousel);
-
         // create div with class="carousel-inner"
         var div_carousel_inner = document.createElement("div");
         div_carousel_inner.setAttribute("class", "carousel-inner");
@@ -80,7 +59,7 @@ function picture_carousel(dish){
                 var new_image = document.createElement("img");
                 //var image_url = "../images/" + name_country + "/" + dish_name;
                 new_image.setAttribute("src", "../images/" + name_country + "/" + dish_name + "/" + dish_name + " " + image + ".jpg");
-                new_image.setAttribute("class", "d-block w-100 h-100");
+                new_image.setAttribute("class", "d-block w-100");
                 new_image.setAttribute("alt", dish_name + " " + image);
 
                 // append image to div
@@ -145,8 +124,6 @@ function picture_carousel(dish){
 
     // append div to new_col
     new_col.appendChild(food_carousel);
-    
-    
 
     return new_col
 }
@@ -154,23 +131,89 @@ function picture_carousel(dish){
 // creates dish information
 function create_dish_info(dish){
     var dish_col = document.createElement("div");
-    dish_col.setAttribute("class", "col-6");
+    dish_col.setAttribute("class", "col-xxl-6 col-xl-12 p-4");
 
+    console.log(dish);
 
+    // create title row with cuisine tag
+    var dish_title_row = document.createElement("div");
+    dish_title_row.setAttribute("class", "row");
+
+        // create dish title in title div to float left
+        var dish_title_div = document.createElement("div");
+        dish_title_div.setAttribute("class", "col-fluid float-left");
+            var dish_title_h2 = document.createElement("h2");
+            dish_title_h2.setAttribute("class", "text-dark");
+            dish_title_h2.textContent = dish["name"];
+        dish_title_div.appendChild(dish_title_h2);
+
+        // *** add start button here in future update ***
+
+        // create button for cuisine name and append it to dish_col
+        var dish_cuisine_div = document.createElement("div");
+        dish_cuisine_div.setAttribute("class", "col float-right");
+            var dish_cuisine_a = document.createElement("a");
+            dish_cuisine_a.setAttribute("class", "btn btn-outline-warning bg-light text-dark disabled");
+            dish_cuisine_a.textContent = dish["cuisine"];
+        dish_cuisine_div.appendChild(dish_cuisine_a);
+
+    // append everything in title row to dish_col
+    dish_title_row.appendChild(dish_title_div);
+    dish_title_row.appendChild(dish_cuisine_div);
+    dish_col.appendChild(dish_title_row);
+
+    // add horizontal line
+    dish_col.appendChild(document.createElement("hr"));
+
+    // create defining characteristics
+    var dish_def_char_h4 = document.createElement("h4");
+    dish_def_char_h4.textContent = "Defining Characteristics";
+    var dish_def_char_p = document.createElement("p");
+    dish_def_char_p.setAttribute("class", "border border-warning bg-light p-3");
+    dish_def_char_p.textContent = dish["defining_char"];
+    // append the defining char title and the content
+    dish_col.appendChild(dish_def_char_h4);
+    dish_col.appendChild(dish_def_char_p);
+
+    // create impressions
+    var dish_impressions_h4 = document.createElement("h4");
+    dish_impressions_h4.textContent = "Impressions";
+    var dish_impressions_p = document.createElement("p");
+    dish_impressions_p.setAttribute("class", "border border-warning bg-light p-3");
+    dish_impressions_p.textContent = dish["impressions"];
+    // append the defining char title and the content
+    dish_col.appendChild(dish_impressions_h4);
+    dish_col.appendChild(dish_impressions_p);
+
+    // add break
+    dish_col.appendChild(document.createElement("br"));
+
+    // create button with link to favorite recipe
+    var dish_recipe_a = document.createElement("a");
+    dish_recipe_a.setAttribute("class", "btn btn-warning");
+    dish_recipe_a.setAttribute("href", dish["recipe"]);
+    dish_recipe_a.setAttribute("role", "button");
+    dish_recipe_a.textContent = "Favorite Recipe";
+    dish_col.appendChild(dish_recipe_a);
 
     return dish_col
 }
 
 //creates section for dish
 function create_section(dish_data){
+    var section_container = document.createElement("div");
+    section_container.setAttribute("class", "container-fluid background-yellow rounded p-0 my-5 center-block shadow-lg");
+    //section_container.setAttribute("style", "border: 0.05rem solid black; box-shadow: 0.50rem 0.50rem 0.25rem 0.25rem #969696");
+
     var new_row = document.createElement("div");
-    new_row.setAttribute("class", "row");
+    new_row.setAttribute("class", "row justify-content-evenly m-auto align-center");
 
     new_row.appendChild(picture_carousel(dish_data));
-
     new_row.appendChild(create_dish_info(dish_data));
 
-    return new_row
+    section_container.appendChild(new_row)
+
+    return section_container
 }
 
 function main(data){
